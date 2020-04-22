3a08818383d2477526e5f77cf1f1c9749f3e6950
Nandhini <nlakuduva@gmail.com>
2020-04-22 12:14:32 -0500

    removed extra lstm in word decoder

--- project/models.py
+++ project/models.py
@@ -76,7 +76,7 @@ class WordDecoder(nn.Module):
     def __init__(self, vocab_size, hidden_size, img_feature_size, embedd_size=256):
         super(WordDecoder, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedd_size)
-        self.lstm = nn.LSTM(input_size=embedd_size, hidden_size=hidden_size, batch_first=True)
+        # self.lstm = nn.LSTM(input_size=embedd_size, hidden_size=hidden_size, batch_first=True)
         # self.out =  nn.Linear(hidden_size, vocab_size, bias=True)
         self.adaptive = AdaptiveBlock(embedd_size, hidden_size, vocab_size, img_feature_size)
         # if torch.cuda.device_count() > 1:
@@ -88,9 +88,9 @@ class WordDecoder(nn.Module):
         embeddings = self.embedding(report)
         x = torch.cat((embeddings, v_g.unsqueeze(1), topic_vector), dim=1)
 
-        h_t, cells = self.lstm(x)
+        # h_t, cells = self.lstm(x)
         # scores = self.out(h_t)
-        scores, atten_weights, beta = self.adaptive(x, h_t, cells, V)
+        scores, atten_weights, beta = self.adaptive(x, V)
 
         return scores[:, -1] #, atten_weights, beta
 
